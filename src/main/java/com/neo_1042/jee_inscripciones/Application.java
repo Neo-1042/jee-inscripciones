196c57164a407a1e205c30c913b9dee4301f218a
Neo-1042 <rafaelh.71642@gmail.com>
2025-11-11 00:41:31 -0600

    Add Application.java main class. First mvn package > java -cp workflow

---
+++ src/main/java/com/neo_1042/jee_inscripciones/Application.java
@@ -0,0 +1,15 @@
+package com.neo_1042.jee_inscripciones;
+
+public class Application {
+
+    public Application() {
+        System.out.println("Inside Application's constructor");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world from my JEE INSCRIPCIONES web app");
+
+        Application app = new Application();
+    }
+}
